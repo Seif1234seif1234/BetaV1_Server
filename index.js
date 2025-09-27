08d576a29db7cbe63af6107d9fd5d7a9cdb94235
Seif1234seif1234 <126608064+Seif1234seif1234@users.noreply.github.com>
2025-09-27 16:00:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const session = require('express-session')
-const RedisStore = require('connect-redis').default
+const { RedisStore } = require('connect-redis').default
 const { createClient } = require('redis')
 const rate = require('express-rate-limit')
 const cors = require('cors')
